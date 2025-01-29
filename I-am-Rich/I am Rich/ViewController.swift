edae45ccf52409283f094b23b4aa208cd2268a27
BHAVYA MISTRY <bhavya.mstry@gmail.com>
2025-01-28 21:26:30 -0500

    I am Rich Project

---
+++ I-am-Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Bhavya Mistry on 1/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
